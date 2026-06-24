d23b2888817e2cb6106aba34e1d30d623fdff8cd
canine89 <hgpark@goldenrabbit.co.kr>
2026-06-24 15:44:22 +0900

    Add oh-my-grid cask

---
+++ Casks/oh-my-grid.rb
@@ -0,0 +1,20 @@
+cask "oh-my-grid" do
+  version "1.6.1"
+  sha256 "7834966a1635eaaae84bf7afa69098a2d5b701f7b90819b1abba6cb2e46c7cd3"
+
+  url "https://github.com/Canine89/oh-my-grid/releases/download/v#{version}/oh-my-grid-#{version}.dmg"
+  name "oh-my-grid"
+  desc "Grid-based macOS window snapping tool"
+  homepage "https://github.com/Canine89/oh-my-grid"
+
+  depends_on macos: ">= :tahoe"
+
+  app "oh-my-grid.app"
+
+  zap trash: [
+    "~/Library/Application Support/oh-my-grid",
+    "~/Library/Caches/com.goldenrabbit.ohmygrid",
+    "~/Library/HTTPStorages/com.goldenrabbit.ohmygrid",
+    "~/Library/Preferences/com.goldenrabbit.ohmygrid.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# Canine89 Homebrew Tap
+
+Homebrew tap for Canine89 macOS apps.
+
+## Install oh-my-grid
+
+```sh
+brew install --cask canine89/tap/oh-my-grid
+```
+
+Or tap first:
+
+```sh
+brew tap canine89/tap
+brew install --cask oh-my-grid
+```
